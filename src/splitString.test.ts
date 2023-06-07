8c032b617c334c13cf97123d0c1b7bd52b980a5f
Zichao Zang <zang.code@gmail.com>
2023-06-07 13:17:38 +0100

    Solve kata and add more tests

--- src/splitString.test.ts
+++ src/splitString.test.ts
@@ -6,6 +6,10 @@ describe("test split string function", () => {
   });
   it("should split strings longer than 2 characters", () => {
     expect(splitString('abcd')).toStrictEqual(['ab', 'cd']);
+    expect(splitString('abcdefgh')).toStrictEqual(['ab', 'cd', 'ef', 'gh']);
   });
-
+  it("should add underscore for string of odd length", () => {
+    expect(splitString('abcde')).toStrictEqual(['ab', 'cd', 'e_']);
+    expect(splitString('a')).toStrictEqual(['a_']);
+  });
 });
--- src/splitString.ts
+++ src/splitString.ts
@@ -1,7 +1,11 @@
 export function splitString(str: string): Array<string> {
-  const returnArr : Array<string> = [];
-  if (str.length > 2) {
-    str.split("", 2)
+  if(str.length % 2 != 0) {
+    str += '_'
   }
-  return ['ab']
+
+  const twoCharArr: Array<string> | null = str.match(/.{1,2}/g)
+
+  if (twoCharArr === null) return [];
+
+  return twoCharArr
 }
